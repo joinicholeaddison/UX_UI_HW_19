9a41f5b3489bcebf924c9050a8ee4b657da371fd
joinicholeaddison <99057750+joinicholeaddison@users.noreply.github.com>
2022-08-10 22:25:08 -0400

    update

---
+++ js/index.js
@@ -0,0 +1,83 @@
+var professional = document.getElementById('professional'); 
+var professional_body = document.getElementById('Professional_body');
+
+var work = document.getElementById('work'); 
+var work_body = document.getElementById('work_body');
+
+var education = document.getElementById('education'); 
+var education_body = document.getElementById('education_body');
+
+var skills = document.getElementById('skills'); 
+var skills_body = document.getElementById('skills_body');
+
+var tools = document.getElementById('tools'); 
+var tools_body = document.getElementById('tools_body');
+
+console.log("hello")
+
+
+
+var show_professional = function () { 
+    work_body.classList.add('hide');
+    professional_body.classList.remove('hide');
+   education_body.classList.add('hide');
+   skills.classList.add('hide');
+   tools.classList.add('hide');
+} 
+
+
+professional.addEventListener('click', show_professional)
+
+var show_work = function () { 
+   professional_body.classList.add('hide');
+    work_body.classList.remove('hide');
+    education_body.classList.add('hide');
+    skills.classList.add('hide');
+    tools.classList.add('hide');} 
+
+
+
+work.addEventListener('click', show_work)
+
+var show_work = function () { 
+    professional_body.classList.add('hide');
+     work_body.classList.remove('hide');
+     education_body.classList.add('hide');
+     skills.classList.add('hide');
+     tools.classList.add('hide');} 
+ 
+
+
+education.addEventListener('click', show_education)
+
+var show_work = function () { 
+    professional_body.classList.add('hide');
+     work_body.classList.add('hide');
+     education_body.classList.remove('hide');
+     skills.classList.add('hide');
+     tools.classList.add('hide');} 
+ 
+
+
+skills.addEventListener('click', show_skills)
+
+var show_work = skills () {
+    professional_body.classList.add ('hide');
+    work_body.classList.add('hide');
+    education_body.classList.add('hide');
+    skills.classList.remove('hide');
+    tools.classList.add('hide');
+} 
+ 
+
+
+
+tools.addEventListener('click', show_tools)
+
+var show_work = function () { 
+    professional_body.classList.add('hide');
+     work_body.classList.add('hide');
+     education_body.classList.add('hide');
+     skills.classList.add('hide');
+     tools.classList.remove('hide');} 
+ 
